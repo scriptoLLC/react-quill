75d68a98bd2b2ae9bf4927e8bd2017244fd0b6be
Todd Kennedy <todd@selfassembled.org>
2017-01-25 12:44:13 -0800

    remove shims[

--- shims/react-dom.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'dev') {
-  module.exports = window.ReactDOM
-} else {
-  module.exports = require('react-dom')
-}
--- shims/react.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'dev') {
-  module.exports = window.React
-} else {
-  module.exports = require('react')
-}
